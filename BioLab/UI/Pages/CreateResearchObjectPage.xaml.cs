180eade7bc392ea8f07ec121a72aa5bc02585522
NukDokPlex <vik.titoff2014@yandex.ru>
2020-11-20 18:04:25 +0500

    Added buttons in menu in StartPage. Developing on CreateResearchObjectPage.

---
+++ BioLab/UI/Pages/CreateResearchObjectPage.xaml.cs
@@ -0,0 +1,127 @@
+﻿using BioLab.Database;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BioLab.UI.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateResearchObjectPage.xaml
+    /// </summary>
+    public partial class CreateResearchObjectPage : Page
+    {
+        private long CurrentPage = 1;
+        private long LastPage = 0;
+        private int EntriesPerPage = 20;
+        private long CurrentEntriesCount = 0;
+        private string CurrentSearchQuery = "";
+        private int CurrentOffset { 
+            get
+            {
+                return unchecked((int)((CurrentPage - 1) * EntriesPerPage));
+            } 
+        }
+        
+        public CreateResearchObjectPage()
+        {
+            InitializeComponent();
+            PaginationControlsToggleEnabledState();
+        }
+
+        private void PaginationControlsToggleEnabledState()
+        {
+            PaginationControlsContainer.IsEnabled = !PaginationControlsContainer.IsEnabled;
+        }
+
+        private void CalculateLastPage(out long LastPage, long EntriesCount, int EntriesPerPage)
+        {
+
+            LastPage = (EntriesCount - 1) / EntriesPerPage + 1;
+        }
+
+        private void RefreshPatients()
+        {
+            List <patient> patients;
+            if (CurrentSearchQuery == string.Empty)
+            {
+                patients = (from patient in App.DB.patients
+                            select patient).Skip(CurrentOffset).Take(EntriesPerPage).ToList();
+                
+                
+            }
+            else
+            {
+                patients = (from patient in App.DB.patients
+                            where DbFunctions.Like(patient.full_name, "*" + CurrentSearchQuery + "*")
+                            select patient).Skip(CurrentOffset).Take(EntriesPerPage).ToList();
+            }
+            patientsDataGrid.Columns.Clear();
+            patientsDataGrid.ItemsSource = patients;
+        }
+
+        
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            RefreshPatients();
+            CalculateLastPage(out LastPage, App.DB.patients.Count(), EntriesPerPage);
+            
+        }
+
+        private void PatientSearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(PatientNameSearchField.Text))
+            {
+                CurrentSearchQuery = PatientNameSearchField.Text;
+                RefreshPatients();
+            }
+            else
+            {
+                MessageBox.Show("Поле поиска не должно быть пустым!", "Ошибка поиска", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        private void CreateResearchObjectButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CurrentPageTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void PatientsFirstPageButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PatientsPreviousPageButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PatientsNextPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PatientsLastPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- BioLab/UI/Pages/StartPage.xaml.cs
+++ BioLab/UI/Pages/StartPage.xaml.cs
@@ -22,43 +22,85 @@ namespace BioLab.UI.Pages
     /// </summary>
     public partial class StartPage : Page
     {
+        private static bool isAuthRemovedFromBackStack = false;
         public StartPage()
         {
+            
             InitializeComponent();
+            DisableDisallowedMenuEntries(App.currentUser.user_type);
         }
 
-        private void Page_Loaded(object sender, RoutedEventArgs e)
+        private void DisableDisallowedMenuEntries(long user_type)
         {
-            
-            MainWindow.header.HeaderTitle.Text = Title;
-            userName.Text = App.currentUser.name;
-            UserImage.Source = new BitmapImage(new Uri(@"\Resources\Images\laborant_"+App.currentUser.user_type.ToString()+".jpeg", UriKind.RelativeOrAbsolute));
+            foreach (Button button in MenuContainer.Children)
+            {
+                button.IsEnabled = user_type == long.Parse(button.Tag.ToString()); 
+                // Just disabling buttons if user type not equals their tags
+            }
+        }
+
+        private void ProcessBackStack()
+        {
+            if (!isAuthRemovedFromBackStack)
+                NavigationService.RemoveBackEntry();
+            isAuthRemovedFromBackStack = true;
+        }
+
+        public void RefreshUserInfo()
+        {
+            userName.Text = App.currentUser.name; // User real name
+
+            UserImage.Source = new BitmapImage(new Uri(@"\Resources\Images\laborant_" + 
+                App.currentUser.user_type.ToString() + ".jpeg", UriKind.RelativeOrAbsolute)); 
+            // User avatars (images are already in project)
+
             userType.Text = (from user_type in App.DB.user_types
-                        where user_type.id == App.currentUser.user_type
-                        select user_type).Single().name;
-            List<long> services = new List<long>();
+                             where user_type.id == App.currentUser.user_type
+                             select user_type).Single().name; // Getting user type name
+
+            List<long> services = new List<long>(); //Warm-up for services json parsing
             if (App.currentUser.services == String.Empty)
             {
-                App.currentUser.services = "[]";
+                App.currentUser.services = "[]"; // Protecting from empty string (JSON converter can't deal with it)
             }
+
             List<BioLab.Database.DataModels.Service> jsonServices = 
-                Newtonsoft.Json.JsonConvert.DeserializeObject<List<BioLab.Database.DataModels.Service>>(App.currentUser.services);
+                Newtonsoft.Json.JsonConvert
+                .DeserializeObject<List<BioLab.Database.DataModels.Service>>(
+                    App.currentUser.services
+                    ); // Converting JSON to POJO
+
             foreach (BioLab.Database.DataModels.Service service in jsonServices)
             {
-                services.Add(service.code);
+                services.Add(service.code); // Retrieving services codes from POJO
             }
+
             List<string> serviceNames = new List<string>();
             serviceNames = (from service in App.DB.services
                             where services.Contains(service.id)
-                            select service.name).ToList();
-            UserServices.Children.Clear();
+                            select service.name).ToList(); // Getting service names by their codes (ids)
+            UserServices.Children.Clear(); // Clearing StackPanel from previous service names
             foreach (string service in serviceNames)
             {
                 TextBlock textBlock = new TextBlock();
                 textBlock.Text = service;
                 textBlock.FontSize = 18;
-                UserServices.Children.Add(textBlock);
+                UserServices.Children.Add(textBlock); // Adding service names (TextBlocks) to StackPanel (Container)
             }
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            ProcessBackStack(); //Deletes AuthForm from Navigation backstack
+
+            MainWindow.header.HeaderTitle.Text = Title; //Setting page title in header
+
+            RefreshUserInfo(); //Update user info in UI (not async, takes UI runtime)
+        }
+
+        private void CreateResearchObjectButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new CreateResearchObjectPage());
         }
     }
 }
